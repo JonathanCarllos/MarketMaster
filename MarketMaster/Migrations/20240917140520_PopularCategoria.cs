63543e50c7d154709b9676ac211b275c6c23abb7
Jonathan Carlos <jonathan.carllos@hotmail.com>
2024-09-25 17:01:34 -0300

    correção feita

--- MarketMaster/Migrations/20240917140520_PopularCategoria.cs
+++ MarketMaster/Migrations/20240917140520_PopularCategoria.cs
@@ -12,7 +12,6 @@ namespace MarketMaster.Migrations
         {
             migrationBuilder.Sql("Insert into Categorias(Nome,Descricao) values('Celular','Um smartphone é um dispositivo eletrônico que combina as funções de um telefone celular com as de um computador de bolso.')");
             migrationBuilder.Sql("Insert into Categorias(Nome,Descricao) values('Eletrodoméstico','aparelhos elétricos que facilitam as tarefas domésticas, como: Cozinhar e conservar alimentos, Limpar a casa, Tratar da roupa, Cuidados de beleza, Entretenimento.')");
-            migrationBuilder.Sql("Insert into Categorias(Nome,Descricao) values('Informática e Acessórios','Procurando mouse sem fio, teclado, gabinete, monitor, pen drive e outros acessórios de informática? Na Americanas tem tuuudo de informática e acessórios para sua casa ou escritório, com os melhores preços e possibilidade de retirar diretamente na loja. Encontre impressoras, cartuchos, toner, scanner, roteador e adaptador wireless, projetores e muitos outros acessórios e peças para computador com descontos imperdíveis na nossa seleção. Vem ver ')");
         }
 
         /// <inheritdoc />
